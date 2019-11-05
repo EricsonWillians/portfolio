82cb612c872f5f629b5198c509cbf156eee5b264
Ericson Willians <ericson.willians@concil.com.br>
2019-11-04 23:56:27 -0300

    Added keydown events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react'
 import './assets/style/main.scss';
-import Synth from './Synth/Synth';
+import Synth from './Synth';
 
-export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    
-  }
-
-  render() {
-    return (
-      <div className="main">
+export default function App() {
+  return (
+    <div className="main">
         <div className="main-container">
-          <p className="app-title">Ericson's Fancy Synth</p>
+          <p className="app-title">Ericson's Synth</p>
           <Synth/>
         </div>
       </div>
-    )
-  }
+  )
 }
---
+++ src/Synth/index.js
@@ -0,0 +1,144 @@
+import React, { Component, useState } from "react";
+import Tone from "tone";
+import "./index.scss";
+import { keyTable } from "./keyTable";
+
+export default function Synth() {
+  const [naturalNotes, setNaturalNotes] = useState([
+    "C3",
+    "D3",
+    "E3",
+    "F3",
+    "G3",
+    "A3",
+    "B3",
+    "C4",
+    "D4",
+    "E4",
+    "F4",
+    "G4",
+    "A4",
+    "B4",
+    "C5"
+  ]);
+
+  const [sharpNotes, setSharpNotes] = useState([
+    "C#3",
+    "D#3",
+    "F#3",
+    "G#3",
+    "A#3",
+    "C#4",
+    "D#4",
+    "F#4",
+    "G#4",
+    "A#4"
+  ]);
+
+  const [duration, setDuration] = useState(0.1);
+  const [oscillator, setOscillator] = useState(
+    new Tone.PolySynth(4, Tone.FMSynth, {
+      oscillator: {
+        type: "triangle"
+      }
+    }).toMaster()
+  );
+
+  return (
+    <div className="synth">
+      <div className="synth__natural__notes">
+        {naturalNotes.map(note => {
+          return (
+            <NaturalNote
+              note={note}
+              duration={duration}
+              oscillator={oscillator}
+            />
+          );
+        })}
+        <div className="synth__sharp__notes">
+          {sharpNotes.map(note => {
+            return (
+              <SharpNote
+                note={note}
+                duration={duration}
+                oscillator={oscillator}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function NaturalNote({ note, duration, oscillator }) {
+  const [triggered, setTriggered] = useState(false);
+  document.addEventListener("keydown", event => {
+    if (event.keyCode === keyTable[note]) {
+      if (!triggered) {
+				oscillator.triggerAttackRelease(note, duration);
+				setTriggered(true);
+			}
+    } 
+	});
+	document.addEventListener("keyup", event => {
+    if (event.keyCode === keyTable[note]) {
+      setTriggered(false);
+    } 
+  });
+  return (
+    <div
+      className={`synth__natural__note ${(() => {
+				if (triggered) {
+					return "synth__note--triggered"
+				}
+				return ''
+			})()}`}
+      onClick={() => {
+        oscillator.triggerAttackRelease(note, duration);
+      }}
+    />
+  );
+}
+
+export function SharpNote({ note, duration, oscillator }) {
+  const [triggered, setTriggered] = useState(false);
+  document.addEventListener("keydown", event => {
+    if (event.keyCode === keyTable[note]) {
+			if (!triggered) {
+				oscillator.triggerAttackRelease(note, duration);
+				setTriggered(true);
+			}
+      
+    } 
+	});
+	document.addEventListener("keyup", event => {
+    if (event.keyCode === keyTable[note]) {
+      setTriggered(false);
+    } 
+  });
+  return (
+    <div
+      className={`synth__sharp__note ${(() => {
+        if (
+          note === "D#3" ||
+          note === "A#3" ||
+          note === "D#4" ||
+          note === "A#4"
+        ) {
+          return "synth__sharp__note--separator";
+        }
+        return "";
+      })()} ${(() => {
+				if (triggered) {
+					return "synth__note--triggered"
+				}
+				return ''
+			})()}`}
+      onClick={() => {
+        oscillator.triggerAttackRelease(note, duration);
+      }}
+    />
+  );
+}
