1de80ec772c61749e12b3f40ad4cd2b82f218ef1
Ericson Willians <ericson.willians@concil.com.br>
2019-11-12 23:15:00 -0300

    Updates

---
+++ precache-manifest.eb6ae996c56654c88d9fa341b7f3ffbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e321f7fba2078802a74677443e668f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2340116ad259e2643a27",
+    "url": "/portfolio/static/css/main.1eb5010f.chunk.css"
+  },
+  {
+    "revision": "cb2c46b4cb0628d2a5ec",
+    "url": "/portfolio/static/js/2.a9aa7dba.chunk.js"
+  },
+  {
+    "revision": "2340116ad259e2643a27",
+    "url": "/portfolio/static/js/main.2c5d45fc.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a723ead14c3e326a36899e9eb2c14ffe.js"
+  "/portfolio/precache-manifest.eb6ae996c56654c88d9fa341b7f3ffbc.js"
 );
 
 self.addEventListener('message', (event) => {
